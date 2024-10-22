4d00c0962f1dbd7da851c03f90a9627c1803aa0e
seongbae15 <pseongbae@gmail.com>
2024-10-23 01:45:31 +0900

    Feat: Add DeepConvNet

--- StudyCodes/ch08_deeplearning/train_deepnet.py
+++ StudyCodes/ch08_deeplearning/train_deepnet.py
@@ -1,8 +1,10 @@
 from dataset.mnist import load_mnist
+from deep_convnet import DeepConvNet
 
 
 def train():
     (x_train, t_train), (x_test, t_test) = load_mnist(flatten=False)
+    network = DeepConvNet()
 
 
 def main():
